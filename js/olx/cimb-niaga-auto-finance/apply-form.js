ba87c36b7ef5b584dfcdd7f34a72a482d1c75bca
Sendy Halim <sendyhalim93@gmail.com>
2016-03-18 10:55:28 +0700

    Refactor, move some functions to module scope

--- js/olx/cimb-niaga-auto-finance/apply-form.js
+++ js/olx/cimb-niaga-auto-finance/apply-form.js
@@ -3,41 +3,39 @@
 var _ = require('lodash/fp');
 var $ = require('jquery');
 
+var inputSelector = _.template('input[name=<%= name %>]');
+var inputValue = function ($form, name) {
+  var selector = inputSelector({
+    name: name
+  });
+  return $form.find(selector).val();
+};
+
+var defaultProductOptions = [
+  {key: 'insuranceType', value: 'All Risk'}
+];
+
+var formData = _.defaults({
+  productId: '56dd564889df9d09ac91b40a', // TODO: hardcode this with CNAF product id in db production.
+  product: 'Retail Financing Mobil Bekas CIMB Niaga Auto Finance',
+  productType: 'Kredit Mobil Bekas',
+  institution: 'CIMB Niaga Auto Finance',
+  url: window.location.href
+});
+
 exports.ready = function () {
   var $form = $('#apply-form');
-  var defaultProductOptions = [
-    {key: 'insuranceType', value: 'All Risk'}
-  ];
-
-  var formData = _.defaults({
-    productId: '56dd564889df9d09ac91b40a', // TODO: hardcode this with CNAF product id in db production.
-    product: 'Retail Financing Mobil Bekas CIMB Niaga Auto Finance',
-    productType: 'Kredit Mobil Bekas',
-    institution: 'CIMB Niaga Auto Finance',
-    url: window.location.href
-  });
-
-  var inputSelector = _.template('input[name=<%= name %>]');
-
-  var inputValue = function (name) {
-    var selector = inputSelector({
-      name: name
-    });
-    return $form.find(selector).val();
-  };
-
-  var productOptions = _.flowRight(
-    defaultProductOptions.concat.bind(defaultProductOptions),
-    _.map(function (key) {
-      return {key: key, value: inputValue(key)};
-    })
-  );
-
   var productOptionNames = [
     'loanAmount',
     'tenure',
     'downPayment'
   ];
+  var productOptions = _.flowRight(
+    defaultProductOptions.concat.bind(defaultProductOptions),
+    _.map(function (key) {
+      return {key: key, value: inputValue($form, key)};
+    })
+  );
 
   var onError = function (response) {
     var errorMessages = _.get('responseJSON.data', response);
@@ -61,10 +59,10 @@ exports.ready = function () {
     event.preventDefault();
 
     var postData = formData({
-      name: inputValue('name'),
-      email: inputValue('email'),
-      city: inputValue('city'),
-      phoneNumber: inputValue('phoneNumber'),
+      name: inputValue($form, 'name'),
+      email: inputValue($form, 'email'),
+      city: inputValue($form, 'city'),
+      phoneNumber: inputValue($form, 'phoneNumber'),
       productOptions: productOptions(productOptionNames)
     });
 
